1206887fe7fd4c0ec7485ad3f8c28184e51554a7
보경 <97583162+bokoo14@users.noreply.github.com>
2023-04-16 00:54:10 +0900

    feat: detail view

---
+++ MyMoodWeather.swiftpm/AboutView.swift
@@ -0,0 +1,20 @@
+//
+//  AboutView.swift
+//  MyMoodWeather
+//
+//  Created by Bokyung on 2023/04/16.
+//
+
+import SwiftUI
+
+struct AboutView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct AboutView_Previews: PreviewProvider {
+    static var previews: some View {
+        AboutView()
+    }
+}
