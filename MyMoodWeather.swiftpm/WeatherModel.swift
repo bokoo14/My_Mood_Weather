9482a231decc657aadee5cd733c0fb6a93395b3a
보경 <97583162+bokoo14@users.noreply.github.com>
2023-04-16 00:51:35 +0900

    feat: mvvm Model

---
+++ MyMoodWeather.swiftpm/WeatherModel.swift
@@ -0,0 +1,14 @@
+//
+//  WeatherModel.swift
+//  MyMoodWeather
+//
+//  Created by Bokyung on 2023/04/15.
+//
+
+import Foundation
+
+struct WeatherModel: Identifiable {
+    let id: String = UUID().uuidString // 사용자의 고유의 UUID값을 생성하고, string으로 변환
+    let name: String // 화면에 표시할 날씨 정보
+    let imageName: String // 이미지의 이름
+}
