66f5bad564bc9b645d7c54fbd39d7bca893d7251
보경 <97583162+bokoo14@users.noreply.github.com>
2023-04-16 17:33:11 +0900

    data: add weather info

---
+++ MyMoodWeather.swiftpm/WeatherInfoView.swift
@@ -0,0 +1,51 @@
+//
+//  WeatherInfoView.swift
+//  MyMoodWeather
+//
+//  Created by Bokyung on 2023/04/16.
+//
+
+import SwiftUI
+
+struct WeatherInfoView: View {
+    // property
+    // @StateObject에서 선언한 viewModel을 @EnvironmentObject를 통해 가져오기
+    @EnvironmentObject var weatherViewModel: WeatherViewModel
+    
+    // property - binding
+    @Environment(\.dismiss) var dismiss
+    
+    var body: some View {
+        ZStack{
+            VStack{
+                ScrollView {
+                    HStack{
+                        Button {
+                            dismiss()
+                        } label: {
+                            Image(systemName: "xmark")
+                                .foregroundColor(.black)
+                                .font(.largeTitle)
+                                .padding()
+                        }
+                        Spacer()
+                    }
+                    
+                    Text("\(weatherViewModel.weatherArray[weatherViewModel.weatherIndex].information)")
+                        .font(.system(size: 30))
+                        .padding(30)
+                    
+                }
+                
+            }
+        }
+    }
+}
+
+struct WeatherInfoView_Previews: PreviewProvider {
+    static var previews: some View {
+        WeatherInfoView()
+            .previewInterfaceOrientation(.landscapeLeft)
+            .environmentObject(WeatherViewModel()) // environmentObject가 강제적으로 WeatherViewModel임을 알려줌
+    }
+}
--- MyMoodWeather.swiftpm/WeatherModel.swift
+++ MyMoodWeather.swiftpm/WeatherModel.swift
@@ -11,4 +11,5 @@ struct WeatherModel: Identifiable {
     let id: String = UUID().uuidString // 사용자의 고유의 UUID값을 생성하고, string으로 변환
     let name: String // 화면에 표시할 날씨 정보
     let imageName: String // 이미지의 이름
+    let information: String // 날씨에 대한 정보
 }
